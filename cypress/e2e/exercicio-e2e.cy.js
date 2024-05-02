4a199f835fe711ff68f2c2de3d5fadaca77a7ff2
Vinicius <vini.arpini10@gmail.com>
2024-05-02 10:47:54 -0400

    Exercicio e2e

---
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+const perfil = require('../fixtures/perfil.json')
+import produtosPage from "../support/page_objects/produtos.page";
+
+context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+  /*  Como cliente 
+      Quero acessar a Loja EBAC 
+      Para fazer um pedido de 4 produtos 
+      Fazendo a escolha dos produtos
+      Adicionando ao carrinho
+      Preenchendo todas opções no checkout
+      E validando minha compra ao final */
+
+  beforeEach(() => {
+      produtosPage.visitarLogin()
+  });
+
+  it('Deve buscar 4 produtos diferentes e adicionar ao carrinho', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+
+    cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        produtosPage.buscarProduto(dados[1].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        produtosPage.buscarProduto(dados[2].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[2].tamanho, dados[2].cor, dados[2].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        produtosPage.buscarProduto(dados[3].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[3].tamanho, dados[3].cor, dados[3].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto)
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
+        cy.get('.checkout-button').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.wait(6000)
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+    })
+   });
+
+});

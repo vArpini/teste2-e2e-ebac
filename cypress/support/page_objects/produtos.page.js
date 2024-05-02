51e7952d1760e4d5648cea6a731315933c48ca23
Vinicius <vini.arpini10@gmail.com>
2024-05-02 10:49:10 -0400

    Teste

---
+++ cypress/support/page_objects/produtos.page.js
@@ -0,0 +1,25 @@
+class ProdutosPage {
+
+    visitarUrl(){
+        cy.visit('produtos')
+    }
+
+    visitarLogin(){
+        cy.visit('minha-conta')
+    }
+
+    buscarProduto(nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    addProdutoCarrinho(tamanho, cor, quantidade){
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+
+}
+
+export default new ProdutosPage()
